3fd0eb15e6d3bd494a75671b1fbfe8cece8f88ea
bhavindewani99 <bhavindewani@gmail.com>
2024-11-15 18:39:03 -0800

    Initial commit for clubconnect-event-club-service

---
+++ src/main/java/com/clubconnect/clubconnect_event_club_service/ClubconnectEventClubServiceApplication.java
@@ -0,0 +1,13 @@
+package com.clubconnect.clubconnect_event_club_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubconnectEventClubServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubconnectEventClubServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clubconnect/clubconnect_event_club_service/ClubconnectEventClubServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clubconnect.clubconnect_event_club_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubconnectEventClubServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
